8ed716f774690edd66177031a70bdb71cb6ab49a
Hadley Callaway <hccallaway@gmail.com>
2018-06-12 13:09:19 -0400

    working on ls

--- bash.js
+++ bash.js
@@ -1,12 +1,15 @@
 // output a prompt
 process.stdout.write('prompt> ');
 
-process.stdin.on('data', (data) => {
+process.stdin.on('data', data => {
   const cmd = data.toString().trim();
   process.stdout.write('You typed: ' + cmd);
   process.stdout.write('\n');
+  const pwd = require('./pwd');
+  const ls = require('./ls');
   if (cmd === 'pwd') {
-  process.stdout.write(__filename);
-}
-  process.stdout.write('\nprompt> ');
-})
+    pwd();
+  } else if (cmd === 'ls') {
+    ls();
+  }
+});
---
+++ pwd.js
@@ -0,0 +1,6 @@
+module.exports = function() {
+  process.stdout.write(__filename);
+  process.stdout.write('\nprompt> ');
+};
+
+// module.exports = forPWD(userInput);
